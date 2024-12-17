43746781378a7176a92b77ede653d72aec0a6c12
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2024-12-17 11:40:55 +0530

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,9 @@
+const str = 123;
+const reverse=str.toString().split("").reverse().join("");
+console.log(reverse)
+const str2="abcdefg";
+const str3=["a","b","c"]
+const data=str2.slice(0,2)
+const data2=str3.splice(1,2)
+console.log(data)
+console.log(data2)
